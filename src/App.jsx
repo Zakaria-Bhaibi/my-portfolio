302e3aa4bf59359293dfb4941cc3bc7203f5f3c1
Zakaria Bhaibi <bhaibizakaria@gmail.com>
2023-12-18 20:23:04 +0100

    innit

--- src/App.jsx
+++ src/App.jsx
@@ -11,41 +11,32 @@ function App() {
         {/*Hero Container */}
         <div id="container" className="mx-auto px-6 py-12 container">
           {/*Nav/Logo Container */}
-          <nav className="flex items-center justify-between font-bold text-white">
+          <nav className="flex items-center justify-between font-alata text-white">
             {/*Logo */}
             <img id="logo" src="logo-desktop.png" alt="" className="w-[200px] h-[80px]"/>
             {/*Menu */}
             <div className="hidden h-10 font-alata md:flex md:space-x-8 md:mr-8">
               <div className="group">
-                <a href="#">About</a>
+                <a href="#">ABOUT</a>
                 <div className="mx-2 group-hover:border-b group-hover:border-blue-50 ">
                 </div>
               </div>
               <div className="group">
-                <a href="#">Carrers</a>
+                <a href="#">SKILLS</a>
                 <div className="mx-2 group-hover:border-b group-hover:border-blue-50 ">
                 </div>
               </div>
               <div className="group">
-                <a href="#">Events</a>
+                <a href="#">PROJECTS</a>
                 <div className="mx-2 group-hover:border-b group-hover:border-blue-50 ">
                 </div>
               </div>
               <div className="group">
-                <a href="#">Products</a>
+                <a href="#">CONTACT</a>
                 <div className="mx-2 group-hover:border-b group-hover:border-blue-50 ">
                 </div>
               </div>
-              <div className="group">
-                <a href="#">Support</a>
-                <div className="mx-2 group-hover:border-b group-hover:border-blue-50 ">
-                </div>
-              </div>
-              <div className="group">
-                <a href="#">Resume</a>
-                <div className="mx-2 group-hover:border-b group-hover:border-blue-50 ">
-                </div>
-              </div>
+              
             </div>
           </nav>
         </div>
